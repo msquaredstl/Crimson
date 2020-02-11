e68b0135ed20cb807e89d1333ff3aef5f8889e82
msquaredstl <60911029+msquaredstl@users.noreply.github.com>
2020-02-10 23:15:29 -0600

    Initial Commit

---
+++ Controller/Customer/index.php
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+<?php 
+namespace Msquared\CustPriceRange\Controller\Customer;  
+class Index extends \Magento\Framework\App\Action\Action { 
+    public function execute() { 
+        $this->_view->loadLayout(); 
+        $this->_view->renderLayout(); 
+    } 
+} 
+=======
+<?php 
+namespace Msquared\CustPriceRange\Controller\Customer;  
+class Index extends \Magento\Framework\App\Action\Action { 
+    public function execute() { 
+        $this->_view->loadLayout(); 
+        $this->_view->renderLayout(); 
+    } 
+} 
+>>>>>>> 2db5e2004d1971edbece4a141618afda67190cd4
+?>
