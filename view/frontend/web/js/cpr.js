219b406c8672f1669e85b51790746dd1f7362ef8
msquaredstl <60911029+msquaredstl@users.noreply.github.com>
2020-02-12 16:00:15 -0600

    update 2-12

--- view/frontend/web/js/cpr.js
+++ view/frontend/web/js/cpr.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require([
     'jquery',
     'mage/mage'
@@ -16,23 +15,4 @@ require([
       return false;
    };
  
-=======
-require([
-    'jquery',
-    'mage/mage'
-], function($){
- 
-   var dataForm = $('#pricerange');
-   dataForm.mage('validation', {});
-   
-   var lowrange = dataForm.("lowrange");
-   var highrange = dataForm.("highrange");
-   
-   if (highrange >= lowrange) || (highrange <= (lowrange * 5)) {
-      return true;
-   } else {
-      return false;
-   };
- 
->>>>>>> 2db5e2004d1971edbece4a141618afda67190cd4
 });
