ed379bf33e951577bf7e43bf6fd590bd60025e17
msquaredstl <60911029+msquaredstl@users.noreply.github.com>
2020-02-15 00:41:51 -0600

    update 2-14

--- Block/CustPriceRange.php
+++ Block/CustPriceRange.php
@@ -3,11 +3,45 @@ namespace Msquared\CustPriceRange\Block;
 class CustPriceRange extends \Magento\Framework\View\Element\Template
 
 {    
+   protected $_productCollectionFactory;
    
-   public function getFormAction()
-   {
-     return '/custpricerange/customer/results';
-   }
-      
+    public function __construct(
+       \Magento\Backend\Block\Template\Context $context,
+       \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+       \Magento\Catalog\Model\Product\Attribute\Source\Status $productStatus,
+       \Magento\Catalog\Model\Product\Visibility $productVisibility,
+       \Magento\Framework\Registry $registry,
+       array $data = []
+     )
+     {
+       $this->_productCollectionFactory = $productCollectionFactory;
+       parent::__construct($context, $data);
+     }
+   
+     public function getProductCollection()
+     {
+       try {
+
+         $collection = $this->_productCollectionFactory->create();
+         $collection->addAttributeToSelect('*')
+             ->addPriceDataFieldFilter('%s >= %s', ['final_price', $price_from])
+             ->addPriceDataFieldFilter('%s <= %s', ['final_price', $price_to])
+             ->addFinalPrice()
+             ->setPageSize(10);
+
+         if ($sortbyprice == 'D') {
+            $collection->getSelect()->order('price_index.final_price DSC');
+         } elseif ($sortbyprice == 'A') {
+             $collection->getSelect()->order('price_index.final_price ASC');
+         }
+
+         return $collection;
+
+       } catch (\Exception $e) {
+
+         var_dump($e->getMessage());
+       }
+     }
+   
 }
 ?>
--- Controller/Customer/index.php
+++ Controller/Customer/index.php
@@ -1,9 +1,30 @@
 <?php 
 namespace Msquared\CustPriceRange\Controller\Customer;  
+
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\Result\JsonFactory;
+
 class Index extends \Magento\Framework\App\Action\Action { 
+   
+
     public function execute() { 
-        $this->_view->loadLayout(); 
+
+        $post = (array) $this->getRequest()->getPost();
+
+        if (!empty($post)) {
+            $lowrange     = $post['lowrange'];
+            $highrange    = $post['highrange'];
+            $sortbyprice  = $post['sortbyprice'];
+
+            $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+
+            return $resultJson;
+        }
+ 
+        $this->_view->loadLayout();
         $this->_view->renderLayout(); 
+    
     } 
 } 
 
